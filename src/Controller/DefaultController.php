f6854f455422b2a3a798c204b2acf3e441dd8162
Manuel Alcázar <manuzz3112@gmail.com>
2022-07-11 10:50:02 +0200

    añado estilos

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -10,7 +10,9 @@ use App\Form\StadiumCreateType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class DefaultController extends AbstractController{
+class DefaultController extends AbstractController
+{
+
 #[Route("/stadium/{id}", name:"showStadium")]
 public function getStadium($id, EntityManagerInterface $doctrine){
     $repository = $doctrine->getRepository(Stadium::class);
@@ -24,29 +26,7 @@ $repository = $doctrine->getRepository(Stadium::class);
 $stadiums=$repository->findAll();
 return $this-> render("stadium/listStadium.html.twig", ["stadiums"=>$stadiums]);
 }
-// #[Route("/insert/stadium")]
-// public function insertStadium(EntityManagerInterface $doctrine)
-// {
-// $stadium1 =new Stadium();
-// $stadium1 -> setName("San Siro");
-// $stadium1 -> setCity("Milán");
-// $stadium1 -> setCapacity(80018);
-// $stadium1 -> setTeam("A.C Milán");
-// $stadium1 -> setImage("https://res.cloudinary.com/dcpgr4jjn/image/upload/v1657467892/images/sansiro_nhpwv9.jpg");
 
-// $stadium2 =new Stadium();
-// $stadium2 -> setName("Santiago Bernabéu");
-// $stadium2 -> setCity("Madrid");
-// $stadium2 -> setCapacity(81044);
-// $stadium2 -> setTeam("Real Madrid");
-// $stadium2-> setImage("https://res.cloudinary.com/dcpgr4jjn/image/upload/v1657467892/images/bernabeu_ecm2nc.jpg");
-
-// $doctrine->persist($stadium1);
-// $doctrine->persist($stadium2);
-
-// $doctrine->flush();
-// return new Response("Stadiums Actualized");
-// }
 #[Route('/new/stadium', name: "newStadium")]
 public function newStadium(Request $request, EntityManagerInterface $doctrine){
 
@@ -74,6 +54,15 @@ public function editStadium( $id, Request $request, EntityManagerInterface $doct
         return $this-> redirectToRoute('getStadiums');
     }
     return $this-> renderForm("stadium/newStadium.html.twig", ["stadiumForm"=>$form]);
+}
+#[Route("/delete/stadium/{id}", name:"deleteStadium")]
+public function deleteStadium($id, EntityManagerInterface $doctrine){
+    $repository =  $doctrine->getRepository(Stadium::class);
+    $stadium = $repository->find($id);
+
+    $doctrine->remove($stadium);
+    $doctrine->flush();
+    return $this->redirectToRoute('getStadiums');
 }
  }
 ?>
